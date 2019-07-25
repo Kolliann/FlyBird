6299d56a86752c94c6661f6e31d1efc3cf579288
LAPTOP-J77OQKK9\Nikolay <Kollianntut@gmail.com>
2019-07-25 22:17:21 +0300

    Create columns

--- FlyBird/Assets/Scripts/Bird.cs
+++ FlyBird/Assets/Scripts/Bird.cs
@@ -4,21 +4,20 @@ using UnityEngine.Serialization;
 public class Bird : MonoBehaviour
 {
     //how much it takes off
-    public  float UpForce = 200f;
+    public float UpForce = 200f;
 
     //it's die player or no
     public static bool IsDead;
-    [FormerlySerializedAs("_rb2D")] 
-    public  Rigidbody2D Rb2D;
-    [FormerlySerializedAs("_anim")]
-    public  Animator Anim;
+    [FormerlySerializedAs("_rb2D")] public Rigidbody2D Rb2D;
+    [FormerlySerializedAs("_anim")] public Animator Anim;
+
 
     // Start is called before the first frame update
     private void Start()
     {
         //
-       // _rb2D = GetComponent<Rigidbody2D>();
-       // _anim = GetComponent<Animator>();
+        // _rb2D = GetComponent<Rigidbody2D>();
+        // _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,10 +31,10 @@ public class Bird : MonoBehaviour
                 Anim.SetTrigger("Flap");
                 Rb2D.velocity = Vector2.zero;
                 Rb2D.AddForce(new Vector2(0, UpForce));
-                
             }
         }
     }
+
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -44,7 +43,7 @@ public class Bird : MonoBehaviour
         Anim.SetTrigger("Die");
 
         IsDead = true;
-        
+
         GameController.Instance.BirdDie();
     }
 }
--- FlyBird/Assets/Scripts/GameController.cs
+++ FlyBird/Assets/Scripts/GameController.cs
@@ -2,26 +2,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public static GameController Instance;
-    
-    [FormerlySerializedAs("_gameOverText")] 
-    public  GameObject GameOverText;
 
-    [FormerlySerializedAs("_gameOver")]
-    public  bool GameOver = false;
-    
+    [FormerlySerializedAs("_gameOverText")]
+    public GameObject GameOverText;
+
+    [FormerlySerializedAs("_gameOver")] public bool GameOver = false;
+
     public float ScrollSpeed = -1.5f;
-    
+
+    private int score = 0;
+
+    public Text scoreText;
+
     // Start is called before the first frame update
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-        }else if (Instance != this)
+        }
+        else if (Instance != this)
         {
             Destroy(gameObject);
         }
@@ -37,9 +42,18 @@ public class GameController : MonoBehaviour
         }
     }
 
+    public void BirdScore()
+    {
+        if (GameOver)
+            return;
+
+        score++;
+
+        scoreText.text = "Score: " + score;
+    }
+
     public void BirdDie()
     {
-        
         GameOverText.SetActive(true);
         GameOver = true;
     }
