b774b525baae4210306060bcbbe26a01a3757f0f
LAPTOP-J77OQKK9\Nikolay <Kollianntut@gmail.com>
2019-09-18 21:28:19 +0300

    Fixed menu

--- FlyBird/Assets/Scripts/MenuController.cs
+++ FlyBird/Assets/Scripts/MenuController.cs
@@ -9,4 +9,17 @@ public class MenuController : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void ExistGame()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Quit work");
+#endif
+        Application.Quit();
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
